fd52e922e979e614220e3afbdde54c10da8b18ee
Larry Taylor and Nicole Freed <nicole@>
2017-07-11 09:11:52 -0700

    add content to web/index.php, app.php with root route, .htaccess file

---
+++ app/app.php
@@ -0,0 +1,21 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Book.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost:8889;dbname=inventory';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('books' => Book::getAll()));
+    });
+
+    return $app;
+?>
